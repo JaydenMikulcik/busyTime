6e38057d3bc6dfe585b2c3d01c5a06e895a904b9
JaydenMikulcik <jjmikulcik@gmail.com>
2023-02-18 13:35:56 -0700

    Busttttt

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native";
+
+//import Camera from "./components/scanCamera";
+
+function camera() {
+  const devices = useCameraDevices("wide-angle-camera");
+  const device = devices.back;
+
+  if (device == null) return <LoadingView />;
+  return (
+    <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <camera />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
